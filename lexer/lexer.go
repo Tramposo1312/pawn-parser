1f2b4831e613fdc905d621a3b3d3a29e983a34de
Tramposo <tramposo1312@users.noreply.github.com>
2024-09-16 12:42:02 +0100

    Init

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,14 +1,16 @@
+// lexer/lexer.go
+
 package lexer
 
 import (
-	"github.com/Southclaws/pawn-parser/token"
+	"github.com/Tramposo1312/pawn-parser/token"
 )
 
 type Lexer struct {
 	input        string
-	position     int  // current position in input (points to current char)
-	readPosition int  // current reading position in input (after current char)
-	ch           byte // current char under examination
+	position     int
+	readPosition int
+	ch           byte
 }
 
 func New(input string) *Lexer {
@@ -27,26 +29,57 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
-func (l *Lexer) NextToken() (tok token.Token) {
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
 	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQL, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
-		tok = newToken(token.ADD, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.ADD_ASSIGN, Literal: string(ch) + string(l.ch)}
+		} else if l.peekChar() == '+' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.INC, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ADD, l.ch)
+		}
+	case '-':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.SUB_ASSIGN, Literal: string(ch) + string(l.ch)}
+		} else if l.peekChar() == '-' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.DEC, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.SUB, l.ch)
+		}
+	// Add cases for other operators and delimiters
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -56,20 +89,50 @@ func (l *Lexer) NextToken() (tok token.Token) {
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
 		} else if isDigit(l.ch) {
-			tok.Type = token.INT
-			tok.Literal = l.readNumber()
-			return tok
+			return l.readNumber()
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
 		}
-
-		tok = newToken(token.ILLEGAL, l.ch)
 	}
 
 	l.readChar()
-	return
+	return tok
 }
 
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) || isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() token.Token {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	if l.ch == '.' && isDigit(l.peekChar()) {
+		l.readChar()
+		for isDigit(l.ch) {
+			l.readChar()
+		}
+		return token.Token{Type: token.FLOAT, Literal: l.input[position:l.position]}
+	}
+	return token.Token{Type: token.INT, Literal: l.input[position:l.position]}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
 }
 
 func isLetter(ch byte) bool {
@@ -80,24 +143,6 @@ func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func (l *Lexer) readNumber() string {
-	position := l.position
-	for isDigit(l.ch) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func (l *Lexer) skipWhitespace() {
-	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
-		l.readChar()
-	}
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
--- token/token.go
+++ token/token.go
@@ -1,3 +1,5 @@
+// token/token.go
+
 package token
 
 type TokenType string
@@ -7,118 +9,126 @@ type Token struct {
 	Literal string
 }
 
-//nolint
 const (
-	ILLEGAL           TokenType = "ILLEGAL"
-	EOF                         = "EOF"
-	COMMENT                     = "COMMENT"
-	IDENT                       = "IDENT"
-	LIT_INT                     = "INT"
-	LIT_FLOAT                   = "FLOAT"
-	LIT_CHAR                    = "CHAR"
-	LIT_STRING                  = "STRING"
-	OP_ADD                      = "+"
-	OP_SUB                      = "-"
-	OP_MULTIPLY                 = "*"
-	OP_QUO                      = "/"
-	OP_MOD                      = "%"
-	OP_AND                      = "&"
-	OP_OR                       = "|"
-	OP_XOR                      = "^"
-	OP_SHIFT_L                  = "<<"
-	OP_SHIFT_R                  = ">>"
-	OP_AND_NOT                  = "&^"
-	OP_ADD_ASSIGN               = "+="
-	OP_SUB_ASSIGN               = "-="
-	OP_MUL_ASSIGN               = "*="
-	OP_QUO_ASSIGN               = "/="
-	OP_REM_ASSIGN               = "%="
-	OP_AND_ASSIGN               = "&="
-	OP_OR_ASSIGN                = "|="
-	OP_XOR_ASSIGN               = "^="
-	OP_SHL_ASSIGN               = "<<="
-	OP_SHR_ASSIGN               = ">>="
-	OP_AND_NOT_ASSIGN           = "&^="
-	OP_LAND                     = "&&"
-	OP_LOR                      = "||"
-	OP_ARROW                    = "<-"
-	OP_INC                      = "++"
-	OP_DEC                      = "--"
-	OP_EQL                      = "=="
-	OP_LSS                      = "<"
-	OP_GTR                      = ">"
-	OP_ASSIGN                   = "="
-	OP_NOT                      = "!"
-	OP_NEQ                      = "!="
-	OP_LEQ                      = "<="
-	OP_GEQ                      = ">="
-	ELLIPSIS                    = "..."
-	LPAREN                      = "("
-	LBRACK                      = "["
-	LBRACE                      = "{"
-	COMMA                       = ","
-	PERIOD                      = "."
-	RPAREN                      = ")"
-	RBRACK                      = "]"
-	RBRACE                      = "}"
-	SEMICOLON                   = ";"
-	COLON                       = ":"
-	DIRECTIVE                   = "#"
-	CONST                       = "const"
-	STATIC                      = "static"
-	STOCK                       = "stock"
-	PUBLIC                      = "public"
-	NATIVE                      = "native"
-	NEW                         = "new"
-	IF                          = "if"
-	ELSE                        = "else"
-	RETURN                      = "return"
-	FOR                         = "for"
-	CONTINUE                    = "continue"
-	BREAK                       = "break"
-	GOTO                        = "goto"
-	SWITCH                      = "switch"
-	CASE                        = "case"
-	DEFAULT                     = "default"
-	DIR_PRAGMA                  = "pragma"
-	DIR_SECTION                 = "section"
-	DIR_LINE                    = "line"
-	DIR_FILE                    = "file"
-	DIR_INCLUDE                 = "include"
-	DIR_TRYINCLUDE              = "tryinclude"
-	DIR_DEFINE                  = "define"
-	DIR_DEFINED                 = "defined"
-	DIR_UNDEF                   = "undef"
-	DIR_ASSERT                  = "assert"
-	DIR_IF                      = "if"
-	DIR_ELSEIF                  = "elseif"
-	DIR_ELSE                    = "else"
-	DIR_ENDIF                   = "endif"
-	DIR_ERROR                   = "error"
-	DIR_ENDINPUT                = "endinput"
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF               = "EOF"
+	COMMENT           = "COMMENT"
+
+	// Identifiers + literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	FLOAT  = "FLOAT"
+	CHAR   = "CHAR"
+	STRING = "STRING"
+
+	// Operators
+	ADD            = "+"
+	SUB            = "-"
+	MUL            = "*"
+	QUO            = "/"
+	REM            = "%"
+	AND            = "&"
+	OR             = "|"
+	XOR            = "^"
+	SHL            = "<<"
+	SHR            = ">>"
+	AND_NOT        = "&^"
+	ADD_ASSIGN     = "+="
+	SUB_ASSIGN     = "-="
+	MUL_ASSIGN     = "*="
+	QUO_ASSIGN     = "/="
+	REM_ASSIGN     = "%="
+	AND_ASSIGN     = "&="
+	OR_ASSIGN      = "|="
+	XOR_ASSIGN     = "^="
+	SHL_ASSIGN     = "<<="
+	SHR_ASSIGN     = ">>="
+	AND_NOT_ASSIGN = "&^="
+	LAND           = "&&"
+	LOR            = "||"
+	ARROW          = "<-"
+	INC            = "++"
+	DEC            = "--"
+	EQL            = "=="
+	LSS            = "<"
+	GTR            = ">"
+	ASSIGN         = "="
+	NOT            = "!"
+	NEQ            = "!="
+	LEQ            = "<="
+	GEQ            = ">="
+
+	// Delimiters
+	LPAREN    = "("
+	LBRACK    = "["
+	LBRACE    = "{"
+	COMMA     = ","
+	PERIOD    = "."
+	RPAREN    = ")"
+	RBRACK    = "]"
+	RBRACE    = "}"
+	SEMICOLON = ";"
+	COLON     = ":"
+
+	// Keywords
+	BREAK    = "BREAK"
+	CASE     = "CASE"
+	CONST    = "CONST"
+	CONTINUE = "CONTINUE"
+	DEFAULT  = "DEFAULT"
+	DO       = "DO"
+	ELSE     = "ELSE"
+	ENUM     = "ENUM"
+	FOR      = "FOR"
+	GOTO     = "GOTO"
+	IF       = "IF"
+	NEW      = "NEW"
+	RETURN   = "RETURN"
+	SIZEOF   = "SIZEOF"
+	STATIC   = "STATIC"
+	SWITCH   = "SWITCH"
+	WHILE    = "WHILE"
+
+	// Pawn keywords
+	ASSERT   = "ASSERT"
+	DEFINED  = "DEFINED"
+	FORWARD  = "FORWARD"
+	NATIVE   = "NATIVE"
+	OPERATOR = "OPERATOR"
+	PUBLIC   = "PUBLIC"
+	STOCK    = "STOCK"
+	TAGOF    = "TAGOF"
+
+	// Preprocessor directives
+	DIRECTIVE = "#"
 )
 
-var tokens = [...]string{}
-
-/*
-char *sc_tokens[] = {
-         "assert", "*begin", "break", "case", "char", "const", "continue", "default",
-         "defined", "do", "else", "emit", "__emit", "*end", "enum", "exit", "for",
-         "forward", "goto", "if", "native", "new", "operator", "public", "return",
-         "sizeof", "sleep", "state", "static", "stock", "switch", "tagof", "*then",
-         "while",
-         "#assert", "#define", "#else", "#elseif", "#emit", "#endif", "#endinput",
-         "#endscript", "#error", "#file", "#if", "#include", "#line", "#pragma",
-         "#tryinclude", "#undef", "#warning",
-         ";", ";", "-integer value-", "-rational value-", "-identifier-",
-         "-label-", "-string-",
-         "-any value-", "-numeric value-", "-data offset-", "-local variable-",
-         "-function-", "-native function-", "-nonnegative integer-"
-       };
-*/
-
 var keywords = map[string]TokenType{
-	"new": NEW,
+	"break":    BREAK,
+	"case":     CASE,
+	"const":    CONST,
+	"continue": CONTINUE,
+	"default":  DEFAULT,
+	"do":       DO,
+	"else":     ELSE,
+	"enum":     ENUM,
+	"for":      FOR,
+	"goto":     GOTO,
+	"if":       IF,
+	"new":      NEW,
+	"return":   RETURN,
+	"sizeof":   SIZEOF,
+	"static":   STATIC,
+	"switch":   SWITCH,
+	"while":    WHILE,
+	"assert":   ASSERT,
+	"defined":  DEFINED,
+	"forward":  FORWARD,
+	"native":   NATIVE,
+	"operator": OPERATOR,
+	"public":   PUBLIC,
+	"stock":    STOCK,
+	"tagof":    TAGOF,
 }
 
 func LookupIdent(ident string) TokenType {
@@ -126,10 +136,4 @@ func LookupIdent(ident string) TokenType {
 		return tok
 	}
 	return IDENT
-}
-
-var t = []string{
-	0: "one",
-	1: "two",
-	2: "three",
 }
